2e435708472f8a765c13e388866c05e10dc98561
v1993 <v19930312@gmail.com>
2017-05-18 11:37:35 +0300

    Travis autotesting in progress (6)

--- .travis/setup_libs.sh
+++ .travis/setup_libs.sh
@@ -2,8 +2,4 @@ I18N_DIR=I18N
 
 git clone https://github.com/kaishiqi/I18N-Gettext-Supported.git $I18N_DIR
 
-echo "${I18N_DIR}/I18NTest_lua/i18n" "$MYLUALIBS"
-
 cp -r "${I18N_DIR}/I18NTest_lua/i18n" "$MYLUALIBS"
-
-echo "${I18N_DIR}/I18NTest_lua/i18n" "$MYLUALIBS"
