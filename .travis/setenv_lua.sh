4ffccf063a28a3a5e777e4f15fabdb15db5c7cae
v1993 <v19930312@gmail.com>
2017-05-18 11:18:15 +0300

    Travis autotesting in progress (2)

--- .travis/setenv_lua.sh
+++ .travis/setenv_lua.sh
@@ -1,5 +1,6 @@
 export MYLUALIBS="$HOME/.lualibs"
+mkdir "$MYLUALIBS"
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
 bash .travis/setup_lua.sh
 eval `$HOME/.lua/luarocks path`
-export LUA_PATH="$LUA_PATH;$MYLUALIBS/?.lua;$MYLUALIBS/?/init.lua"
+export LUA_PATH="$LUA_PATH;${MYLUALIBS}/?.lua;${MYLUALIBS}/?/init.lua"
--- .travis/setup_libs.sh
+++ .travis/setup_libs.sh
@@ -4,4 +4,6 @@ I18N_DIR=I18N
 
 git clone https://github.com/kaishiqi/I18N-Gettext-Supported.git $I18N_DIR
 
-cp -r "$I18N_DIR/I18NTest_lua/i18n" "$MYLUALIBS"
+echo "$MYLUALIBS"
+
+cp -r "${I18N_DIR}/I18NTest_lua/i18n" "$MYLUALIBS"
